328a75c610ee089cf7a29f60cba3c2843465e1f6
KabilBaskar <kabilbaskar88@gmail.com>
2025-09-22 16:22:05 +0530

    Add files via upload

---
+++ arr_length.js
@@ -0,0 +1,5 @@
+const fruits = ["Apple", "Banana", "Cherry", "Dragonfruit"];
+
+const lengths = fruits.map(str => str.length);
+
+console.log("String Lengths:", lengths);
---
+++ traditional.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+    return a + b;
+  }
+  console.log("Traditional:", add(5, 3));
+  
+  const addArrow = (a, b) => a + b;
+  console.log("Arrow:", addArrow(5, 3));
